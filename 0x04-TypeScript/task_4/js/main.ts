68215b7b0a3204286ebefc560e561d3e6651eafd
Likanos Tegene <likanostegene@gmail.com>
2025-07-20 23:34:29 +0300

    Create main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,31 @@
+// task_4/js/main.ts
+import { Cpp, Java, React } from './subjects';
+import { Teacher } from './subjects/Teacher';
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+export const cTeacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
+};
+
+// Cpp
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
